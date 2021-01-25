9cd9d44a32c7f7dc99372b3cae64e17271e33c7a
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-25 18:39:58 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,7 +31,7 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets
   input_to_index(user_input)
-  index = input_to_index
+  index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index, token = "X")
   else input_to_index(user_input)
